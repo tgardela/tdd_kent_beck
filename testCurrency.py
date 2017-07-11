630329e1f478ad60bdf87c5e13a94907dc8bda38
tgardela <king_nothin@o2.pl>
2017-07-11 17:01:38 +0200

    creation of repo

---
+++ testCurrency.py
@@ -0,0 +1,17 @@
+import unittest
+from .currency import Dollar
+
+
+class TestHelpers(unittest.TestCase):
+
+
+
+    def test_Multiplication(self):
+        Dollar five = Dollar(5)
+        five.times(2)
+        self.assertEqual(10, five.ammount)
+
+
+
+if __name__=='__main__':
+    unittest.main(verbosity=2)
