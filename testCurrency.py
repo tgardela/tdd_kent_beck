076011ecd22a8a6b9601eb762823e0b1fa4177ee
tgardela <king_nothin@o2.pl>
2017-07-11 17:20:45 +0200

    c02 test pass

--- testCurrency.py
+++ testCurrency.py
@@ -8,8 +8,12 @@ class TestHelpers(unittest.TestCase):
 
     def test_Multiplication(self):
         five = Dollar(5)
-        five.times(2)
-        self.assertEqual(10, five.amount)
+        product = five.times(2)
+        self.assertEqual(10, product.amount)
+
+        product = five.times(3)
+        self.assertEqual(15, product.amount)
+
 
 
 
