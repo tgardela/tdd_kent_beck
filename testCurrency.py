153261d65100da5b00e5d79c1c24c5240bda7b50
tgardela <king_nothin@o2.pl>
2017-07-11 17:32:39 +0200

    c03 test fail

--- testCurrency.py
+++ testCurrency.py
@@ -4,8 +4,6 @@ from currency import Dollar
 
 class TestHelpers(unittest.TestCase):
 
-
-
     def test_Multiplication(self):
         five = Dollar(5)
         product = five.times(2)
@@ -15,7 +13,8 @@ class TestHelpers(unittest.TestCase):
         self.assertEqual(15, product.amount)
 
 
-
+    def test_Equity(self):
+        self.assertTrue(Dollar(5).equals(Dollar(5)))
 
 if __name__=='__main__':
     unittest.main(verbosity=2)
