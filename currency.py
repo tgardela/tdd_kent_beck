70df3609cddb73b9c6aa3d5dd607ea19d834beba
tgardela <king_nothin@o2.pl>
2017-07-17 12:04:22 +0200

    chapter 6 code with

--- currency.py
+++ currency.py
@@ -1,30 +1,24 @@
-class Dollar():
+class Money():
     def __init__(self, amount):
         self.amount = amount
 
+
+    def equals(self, money):
+        return self.amount == money.amount
+
+
+    def __eq__(self, money):
+        return self.__dict__ == money.__dict__
+
+
+
+class Dollar(Money):
 
     def times(self, multiplier):
         return Dollar(self.amount * multiplier)
 
 
-    def equals(self, dollar):
-        return self.amount == dollar.amount
-
-    def __eq__(self, dollar):
-        return self.__dict__ == dollar.__dict__
-
-
-class Franc():
-    def __init__(self, amount):
-        self.amount = amount
-
+class Franc(Money):
 
     def times(self, multiplier):
         return Franc(self.amount * multiplier)
-
-
-    def equals(self, franc):
-        return self.amount == franc.amount
-
-    def __eq__(self, franc):
-        return self.__dict__ == franc.__dict__
