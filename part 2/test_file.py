5019aea616cfb750cc578669b2d47440e74bced4
tgardela <king_nothin@o2.pl>
2017-07-21 19:04:49 +0200

    change of file names, creation of testing class TestCaseTest

--- part 2/test_file.py
+++
@@ -1,8 +0,0 @@
-from file_to_test import WasRun
-
-
-test = WasRun('testMethod')
-print(test.wasRun)
-
-test.run()
-print(test.wasRun)
---
+++ part 2/testcasetest.py
@@ -0,0 +1,12 @@
+from testcase import TestCase, WasRun
+
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun('testMethod')
+        assert(not test.wasRun)
+
+        test.run()
+        assert(test.wasRun)
+
+TestCaseTest('testRunning').run()
