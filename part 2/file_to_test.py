2dafbebd3ca09591244d34d2a105cefdf29ff868
tgardela <king_nothin@o2.pl>
2017-07-21 18:56:26 +0200

    refactoring the run method so it can call whatever method we need

--- part 2/file_to_test.py
+++ part 2/file_to_test.py
@@ -1,11 +1,13 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
 
 
     def testMethod(self):
         self.wasRun = 1
 
-
+    #this method takes attributes: self and self.name, and calls a method that has a name == self.name
     def run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
