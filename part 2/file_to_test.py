1cb38c12a46eb3b4590a7cecdfeb9c3f8569dfa3
tgardela <king_nothin@o2.pl>
2017-07-21 18:48:02 +0200

    creation of base file and test files, first tests

---
+++ part 2/file_to_test.py
@@ -0,0 +1,7 @@
+class WasRun:
+    def __init__(self, name):
+        self.wasRun = None
+
+
+    def testMethod(self):
+        pass
---
+++ part 2/test_file.py
@@ -0,0 +1,7 @@
+from file_to_test import WasRun
+
+
+test = WasRun('testMethod')
+print(test.wasRun)
+test.testMethod()
+print(test.wasRun)
