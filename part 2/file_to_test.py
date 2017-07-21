5aa295e7295efe7c6bafd0d4b0d5958731ea0a4d
tgardela <king_nothin@o2.pl>
2017-07-21 19:03:22 +0200

    creation of class TestCase which is parent to WasRun

--- part 2/file_to_test.py
+++ part 2/file_to_test.py
@@ -1,13 +1,20 @@
-class WasRun:
+class TestCase():
+    def __init__(self, name):
+        self.name = name
+
+
+    # takes attributes: self and self.name and calls a method that has a name == self.name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
+class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
-        self.name = name
+        TestCase.__init__(self, name)
 
 
     def testMethod(self):
         self.wasRun = 1
 
-    #this method takes attributes: self and self.name, and calls a method that has a name == self.name
-    def run(self):
-        method = getattr(self, self.name)
-        method()
