7dd415ae2a745923f35456ee49ee44783b563909
tgardela <king_nothin@o2.pl>
2017-07-21 19:33:32 +0200

    adding SetUp method and test for it

--- part 2/testcase.py
+++ part 2/testcase.py
@@ -3,18 +3,26 @@ class TestCase():
         self.name = name
 
 
+    def setUp(self):
+        pass
+
     # takes attributes: self and self.name and calls a method that has a name == self.name
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
 
 
     def testMethod(self):
         self.wasRun = 1
+
+
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
 
--- part 2/testcasetest.py
+++ part 2/testcasetest.py
@@ -4,9 +4,14 @@ from testcase import TestCase, WasRun
 class TestCaseTest(TestCase):
     def testRunning(self):
         test = WasRun('testMethod')
-        assert(not test.wasRun)
-
         test.run()
         assert(test.wasRun)
 
+
+    def testSetUp(self):
+        test = WasRun('testMethod')
+        test.run()
+        assert(test.wasSetUp)
+
 TestCaseTest('testRunning').run()
+TestCaseTest('testSetUp').run()
