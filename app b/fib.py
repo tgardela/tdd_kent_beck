5c1864b73ea12a883a09e98e18bd66497d21ea1f
tgardela <king_nothin@o2.pl>
2017-07-25 09:31:32 +0200

    appendix fibonacchi tdd test adding, fail

--- app b/fib.py
+++ app b/fib.py
@@ -1,3 +1,4 @@
 def fibonacchi(n):
     if n == 0 : return 0
-    return 1
+    if n == 1 : return 1
+    return 2
--- app b/test_fib.py
+++ app b/test_fib.py
@@ -4,7 +4,7 @@ import fib
 
 class TestFibonacchi(unittest.TestCase):
     def test_Fibonacchi(self):
-        testCases = [[0,0], [1,1]]
+        testCases = [[0,0], [1,1], [2,1], [3,2]]
         for i in range(0, len(testCases)):
             self.assertEqual(testCases[i][0], fib.fibonacchi(testCases[i][1]))
 
